16b2641e0f5c262811f188923004e423168584f7
andre12140 <andre_._barbosa@hotmail.com>
2020-03-22 12:38:50 +0000

    Penso que ja esta uma boa parte do inverse Kinematics, Falta fazer para as varias soluçoes e testar se esta correcto
    Cumprimentos,
    André Barbosa

---
+++ homo_matrix_andre.m
@@ -0,0 +1,29 @@
+function [matrix] = homo_matrix(angle,translation,axis)
+
+matrix = zeros(3,4);
+angle_deg = angle*180/pi;
+
+if axis=="x"
+    matrix = [1 0 0 translation(1);
+        0 cosd(angle_deg) -sind(angle_deg) translation(2);
+        0 sind(angle_deg) cosd(angle_deg) translation(3);
+        0 0 0 1];
+    
+elseif axis=="y"
+    matrix = [cosd(angle_deg) 0 sind(angle_deg) translation(1);
+        0 1 0 translation(2);
+        -sind(angle_deg) 0 cosd(angle_deg) translation(3);
+        0 0 0 1];
+    
+elseif axis=="z"
+    matrix = [cosd(angle_deg) -sind(angle_deg) 0 translation(1);
+        sind(angle_deg) cosd(angle_deg) 0 translation(2);
+        0 0 1 translation(3);
+        0 0 0 1];
+    
+else
+    error("homo_matrix: Not a valid input");
+end
+
+end
+
--- inverseKinematics.m
+++ inverseKinematics.m
@@ -19,20 +19,24 @@ s1 = sin(A4);
 s2 = sin(A5);
 s3 = sin(A6);
 
-R_base_tool = [c1*c2*c3-s1*s3, -c3*s1-c1*c2*c3, c1*s2;
+R_base_tool = [c1*c2*c3-s1*s3, -c3*s1-c1*c2*s3, c1*s2;
                c1*s3+c2*c3*s1, c1*c3-c2*s1*s3, s1*s2;
                -c3*s2, s2*s3, c2];
 P_base_tool = [A1;A2;A3];
 
 T_base_tool = [R_base_tool, P_base_tool;
                0, 0, 0, 1];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Calcular a posicao da joint 5
+P_base_wrist = T_base_tool*[-Wrist 0 0 1]';
+P_base_wrist = P_base_wrist(1:3);
 
 %posicao A1 A2 A3   - Base_link ate wrist_link - Testado e a funcionar so
 %dando uma solucao
 %Step 1: Inverse Kinematics for position in first 3 joints
-X0_3 = A1;
-Y0_3 = A2;
-Z0_3 = A3;
+X0_3 = P_base_wrist(1);
+Y0_3 = P_base_wrist(2);
+Z0_3 = P_base_wrist(3);
 
 teta1 = (atan2d(Y0_3, X0_3))*pi/180
 
@@ -78,26 +82,48 @@ teta3 = (aux_phi_3 - 90 + aux_simplified)*pi/180
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Step 2: Forward Kinematics on first 3 joints to get rotation part, R0_3
-T= @(alpha,a,d,theta) [cos(theta)              -sin(theta)              0           a;
-                       sin(theta)*cos(alpha)   cos(theta)*cos(alpha)    -sin(alpha) -sin(alpha)*d;
-                       sin(theta)*sin(alpha)   cos(theta)*sin(alpha)    cos(alpha)  cos(alpha)*d;
-                       0                       0                        0           1];
+% T= @(alpha,a,d,theta) [cos(theta)              -sin(theta)              0           a;
+%                        sin(theta)*cos(alpha)   cos(theta)*cos(alpha)    -sin(alpha) -sin(alpha)*d;
+%                        sin(theta)*sin(alpha)   cos(theta)*sin(alpha)    cos(alpha)  cos(alpha)*d;
+%                        0                       0                        0           1];
+% 
+% %assumindo teta4 = 0
+% Table= [0   0   103  teta1;
+%         0   0   80   0 ;
+%     -pi/2   0   0    teta2;
+%         0   0   0    -pi/2;
+%         0  210  0    teta3;
+%     -pi/2  30   0    0;
+%         0  0   221.5  0;];
+%     
+% Transf= eye(4);    
+% for i=1:size(Table,1)
+%     Transf=Transf* T(Table(i,1),Table(i,2),Table(i,3),Table(i,4));
+% end
+% 
+% R0_3 = Transf(1:3,1:3);
 
-%assumindo teta4 = 0
-Table= [0   0   103  teta1;
-        0   0   80   0 ;
-    -pi/2   0   0    teta2;
-        0   0   0    -pi/2;
-        0  210  0    teta3;
-    -pi/2  30   0    0;
-        0  0   221.5  0;];
-    
-Transf= eye(4);    
-for i=1:size(Table,1)
-    Transf=Transf* T(Table(i,1),Table(i,2),Table(i,3),Table(i,4));
-end
 
-R0_3 = Transf(1:3,1:3);
+TBase_J1 = [1 0 0 0;
+            0 1 0 0;
+            0 0 1 Base;
+            0 0 0 1];
+
+TJ1_J2 = homo_matrix_andre(teta1,[0,0,Shoulder],"z");
+
+aux_x = Arm * sind((teta2*180/pi));
+aux_z = Arm * cosd((teta2*180/pi));
+TJ2_J3 = homo_matrix_andre(teta2,[aux_x,0,aux_z],"y");
+
+aux_x = Elbow2 * cosd((teta3*180/pi)) + Elbow1 * sind((teta3*180/pi));
+aux_z = -Elbow2 * sind((teta3*180/pi)) + Elbow1 * cosd((teta3*180/pi));
+TJ3_J4 = homo_matrix_andre(teta3,[aux_x,0,aux_z],"y");
+
+TJ4_J5 = homo_matrix_andre(0,[Forearm,0,0],"x");
+
+T0_3 = TBase_J1*TJ1_J2*TJ2_J3*TJ3_J4*TJ4_J5;
+
+R0_3 = T0_3(1:3,1:3);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Step 3: Find the inverse of the Ro_3 matrix
@@ -115,8 +141,6 @@ R3_6 = R0_3\R_base_tool;
 %Step 5: Specify what you want the rotation matrix R0_6 to be
 
 % ZYZ como esta no enunciado
-
-
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -144,8 +168,27 @@ R3_6 = R0_3\R_base_tool;
 % [                             cos(A6)*sin(A5),                            -sin(A5)*sin(A6),          cos(A5)]
 % [ - cos(A4)*sin(A6) - cos(A5)*cos(A6)*sin(A4),   cos(A5)*sin(A4)*sin(A6) - cos(A4)*cos(A6),  sin(A4)*sin(A5)]
 
-
-
+% syms A4 A5 A6
+% 
+% aux_a4 = [1,0,0;
+%           0,cos(A4),-sin(A4);
+%           0,sin(A4),cos(A4)];
+%       
+% aux_a5 = [cos(A5),0,sin(A5);
+%           0, 1, 0;
+%           -sin(A5),0,cos(A5)];
+% 
+% aux_a6 = [1,0,0;
+%           0,cos(A6),-sin(A6);
+%           0,sin(A6),cos(A6)];
+%       
+% Aux = aux_a4*aux_a5*aux_a6
+% 
+% Aux =
+%  
+% [          cos(A5),                           sin(A5)*sin(A6),                             cos(A6)*sin(A5)]
+% [  sin(A4)*sin(A5), cos(A4)*cos(A6) - cos(A5)*sin(A4)*sin(A6), - cos(A4)*sin(A6) - cos(A5)*cos(A6)*sin(A4)]
+% [ -cos(A4)*sin(A5), cos(A6)*sin(A4) + cos(A4)*cos(A5)*sin(A6),   cos(A4)*cos(A5)*cos(A6) - sin(A4)*sin(A6)]
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Step 7: Plug in those variables and use the rotation matrix to solve for
@@ -153,19 +196,20 @@ R3_6 = R0_3\R_base_tool;
 
 
 %Esta aqui algo errado uma vez que R3_6 nao e igual a R3_6_teste
-Teta5 = acos(R3_6(2,3))
+Teta5 = acos(R3_6(1,1))
 
-Teta6 = asin(R3_6(2,2)/(-sin(Teta5)))
-
-Teta4 = acos(R3_6(1,3)/(-sin(Teta5)))
-
-
-
-R3_6_teste = [cos(Teta4)*cos(Teta5)*cos(Teta6)-sin(Teta4)*sin(Teta6), - cos(Teta6)*sin(Teta4)-cos(Teta4)*cos(Teta5)*sin(Teta6), -cos(Teta4)*sin(Teta5);
-                             cos(Teta6)*sin(Teta5),                            -sin(Teta5)*sin(Teta6),          cos(Teta5);
- - cos(Teta4)*sin(Teta6) - cos(Teta5)*cos(Teta6)*sin(Teta4),   cos(Teta5)*sin(Teta4)*sin(Teta6)-cos(Teta4)*cos(Teta6),  sin(Teta4)*sin(Teta5)];
-
+if(Teta5>-1e-6 && Teta5<1e-6)
+    Teta4 = 0
+    Teta6 = acos(R3_6(3,3))
+    
+else
+    Teta6 = asin(R3_6(1,2)/(sin(Teta5)))
+    Teta4 = asin(R3_6(2,1)/(sin(Teta5)))
+end
 
 
+Teste_R0_3 = [          cos(Teta5),                           sin(Teta5)*sin(Teta6),                             cos(Teta6)*sin(Teta5)
+                sin(Teta4)*sin(Teta5), cos(Teta4)*cos(Teta6) - cos(Teta5)*sin(Teta4)*sin(Teta6), - cos(Teta4)*sin(Teta6) - cos(Teta5)*cos(Teta6)*sin(Teta4)
+               -cos(Teta4)*sin(Teta5), cos(Teta6)*sin(Teta4) + cos(Teta4)*cos(Teta5)*sin(Teta6),   cos(Teta4)*cos(Teta5)*cos(Teta6) - sin(Teta4)*sin(Teta6)];
 
 end
