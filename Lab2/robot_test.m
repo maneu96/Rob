f9766dd52813828b4de9f6911e0ecfa3cfa9e187
danieltarita <9cdanielsilva@gmail.com>
2020-05-08 19:08:36 +0100

    Carrega o path e chega ao destino!

--- Lab2/robot_test.m
+++ Lab2/robot_test.m
@@ -1,28 +1,34 @@
 close all;
+clear all;
 
-X=[0;0];
-theta=pi/4;
+%X=[0;0];
+%theta=pi/4;
 phi=0;
 x_path = [0:10;0,1,2,3,4,5,4,5,4,5,4];
 x_path = [0:10;5*sin(0:0.5:5)];
-theta_path = [0];
-K=1;
+load x_path
 
+
+K=1;
 phi_save = [];
 v_save=[];
 theta_save=[];
 error_save=[];
 b_error_save=[];
-
-for i=2:11
+theta_path=[];
+for i=2:length(x_path)
     vector_dif = x_path(:,i-1)-x_path(:,i);
-    theta_path=[theta_path, atan(vector_dif(2)/vector_dif(1))];
+    theta_path=[theta_path, atan2(vector_dif(2),vector_dif(1))];
 end
 
 i=2;
-theta_path = [theta_path, pi/4];
+theta_path = [theta_path, theta_path(end)];
 
-while K<=11
+X=[x_path(1,1);x_path(2,1)];
+theta=theta_path(1);
+
+
+while K<=length(x_path)
     %t=(i-1)/1000;
     %plot(X(1),X(2));
     %hold on;
@@ -32,15 +38,15 @@ while K<=11
 %     if theta>=pi/2
 %         alpha=0;
 %     end
-     if (norm(X(:,i-1)-x_path(:,K))<0.3) %||...
+     if (norm(X(:,i-1)-x_path(:,K))<3) %||...
 %             ((abs(error_save(1,end))<=0.1) && (abs(theta)<=0.1||(abs(theta)<=pi+0.1 && abs(theta)>=pi-0.1))) ||...
 %             ((abs(error_save(2,end))<=0.1) && (abs(theta)<=pi/2+0.1 || abs(theta)>=pi/2-0.1))
         
-        K=K+1;
-        figure();
-        scatter(x_path(1,:),x_path(2,:));
-        hold on;
-        plot(X(1,:),X(2,:));
+        K=K+1
+        %figure();
+        %scatter(x_path(1,:),x_path(2,:));
+        %hold on;
+        %plot(X(1,:),X(2,:));
     end
 
     [phi,v,error_vec,b_error] = controlador(X(:,i-1),theta, x_path(:,K), theta_path(K+1), phi);
